cec54ab4a28491a3f0d5fed5c484da6a16e9604a
hlthu <huanglu.th@gmail.com>
2016-08-01 17:06:54 +0800

    nothing

--- flower_classify/main.py
+++ flower_classify/main.py
@@ -18,10 +18,11 @@ name = genfromtxt('iris.txt', delimiter=',', usecols=(4), dtype=str)
 # get the train data and test data
 names = ['setosa', 'versicolor', 'virginica']
 t = zeros(len(name))
-t[name == 'setosa'] = 1
-t[name == 'versicolor'] = 2
-t[name == 'virginica'] = 3
+t[name == 'setosa'] = 0
+t[name == 'versicolor'] = 1
+t[name == 'virginica'] = 2
 train, test, train_t, test_t = cross_validation.train_test_split(data, t, test_size=0.4, random_state=0)
+
 
 # train
 classifier = GaussianNB()
@@ -32,4 +33,3 @@ t_predict = classifier.predict(test)
 ac = sum(test_t == t_predict)
 ac_rate = float(ac)/float(len(test_t))
 print ac_rate
-
