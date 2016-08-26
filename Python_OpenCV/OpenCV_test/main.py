c455bb607d92ef998cf6d226edeb6e0e53a039d2
= <=>
2016-08-26 19:40:26 +0800

    add OpenCV_test

--- Python_OpenCV/OpenCV_test/main.py
+++ Python_OpenCV/OpenCV_test/main.py
@@ -3,19 +3,11 @@ import numpy as np
   
 img = cv2.imread("./cat.png")  
 emptyImage = np.zeros(img.shape, np.uint8)  
-  
-emptyImage2 = img.copy()  
-  
-emptyImage3=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)  
-#emptyImage3[...]=0  
+emptyImage2=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)   
   
 cv2.imshow("EmptyImage", emptyImage)  
 cv2.imshow("Image", img)  
-cv2.imshow("EmptyImage2", emptyImage2)  
-cv2.imshow("EmptyImage3", emptyImage3)  
-cv2.imwrite("./cat2.png", img, [int(cv2.IMWRITE_JPEG_QUALITY), 5])  
-cv2.imwrite("./cat3.png", img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])  
-cv2.imwrite("./cat.png", img, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])  
-cv2.imwrite("./cat2.png", img, [int(cv2.IMWRITE_PNG_COMPRESSION), 9])  
+cv2.imshow("EmptyImage2", emptyImage2)   
+ 
 cv2.waitKey (0)  
 cv2.destroyAllWindows()  
